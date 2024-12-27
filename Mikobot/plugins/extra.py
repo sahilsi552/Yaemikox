5e7b68ff06bb1bca6adda32cd0c58599cf8a48cc
SAHIL SURYAVANSHI <sahilsingh56701@gmail.com>
2024-12-27 12:27:40 +0530

    Update extra.py

--- Mikobot/plugins/extra.py
+++ Mikobot/plugins/extra.py
@@ -1,11 +1,8 @@
 from time import gmtime, strftime, time
+from pyrogram import Client, filters
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
 
-from pyrogram import filters
-from pyrogram.types import Message
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
-from telegram.ext import CallbackQueryHandler, CommandHandler, ContextTypes
-
-from Mikobot import LOGGER, app, function
+from Mikobot import LOGGER, app
 from Mikobot.plugins.helper_funcs.chat_status import check_admin
 
 # Uptime to check how long the bot has been running
@@ -14,7 +11,7 @@ UPTIME = time()
 
 # ID Command
 @app.on_message(filters.command("id"))
-async def _id(client, message: Message):
+async def id_command(client: Client, message: Message):
     chat = message.chat
     your_id = message.from_user.id
     mention_user = message.from_user.mention
@@ -49,55 +46,47 @@ async def _id(client, message: Message):
 
 
 # Logs Command
-@check_admin(only_dev=True)
-async def logs(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    user = update.effective_user
+@app.on_message(filters.command("logs") & check_admin(only_dev=True))
+async def logs_command(client: Client, message: Message):
     with open("Logs.txt", "rb") as f:
         caption = "Here is your log"
         reply_markup = InlineKeyboardMarkup(
             [[InlineKeyboardButton("Close", callback_data="close")]]
         )
-        message = await context.bot.send_document(
+        sent_message = await client.send_document(
+            chat_id=message.chat.id,
             document=f,
-            filename=f.name,
             caption=caption,
             reply_markup=reply_markup,
-            chat_id=user.id,
         )
-        context.user_data["log_message_id"] = message.message_id
+        app.user_data[message.chat.id] = sent_message.id
 
 
-@check_admin(only_dev=True)
-async def close_callback(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    query = update.callback_query
-    message_id = context.user_data.get("log_message_id")
+@app.on_callback_query(filters.regex("^close$") & check_admin(only_dev=True))
+async def close_callback(client: Client, callback_query):
+    message_id = app.user_data.get(callback_query.message.chat.id)
     if message_id:
-        await context.bot.delete_message(
-            chat_id=query.message.chat_id, message_id=message_id
-        )
+        await callback_query.message.delete()
 
 
 # PyroPing Command
 @app.on_message(filters.command("pyroping"))
-async def ping(_, m: Message):
-    LOGGER.info(f"{m.from_user.id} used ping cmd in {m.chat.id}")
+async def ping(client: Client, message: Message):
+    LOGGER.info(f"{message.from_user.id} used ping cmd in {message.chat.id}")
     start = time()
-    replymsg = await m.reply_text(text="Pinging...", quote=True)
+    replymsg = await message.reply_text(text="Pinging...", quote=True)
     delta_ping = time() - start
 
     up = strftime("%Hh %Mm %Ss", gmtime(time() - UPTIME))
     image_url = "https://telegra.ph/file/e1049f371bbec3f006f3a.jpg"
 
-    await replymsg.reply_photo(
+    await client.send_photo(
+        chat_id=message.chat.id,
         photo=image_url,
         caption=f"<b>Pyro-Pong!</b>\n{delta_ping * 1000:.3f} ms\n\nUptime: <code>{up}</code>",
     )
     await replymsg.delete()
 
-
-# Handlers
-function(CommandHandler("logs", logs, block=False))
-function(CallbackQueryHandler(close_callback, pattern="^close$", block=False))
 
 # Help Text
 __help__ = """
