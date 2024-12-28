62f66bc8c4c8d5009b5c5840bc3561e1bf91a3e2
SAHIL SURYAVANSHI <sahilsingh56701@gmail.com>
2024-12-28 17:51:02 +0530

    Update bug.py

--- Mikobot/plugins/bug.py
+++ Mikobot/plugins/bug.py
@@ -1,21 +1,19 @@
 from pyrogram import filters
 from pyrogram.enums import ParseMode
 from variables import Config
-
-
 from Mikobot import app
 
 
 # Define a handler for the /bug command
 @app.on_message(filters.command("bug", prefixes="/"))
-def bug_command_handler(client, message):
+async def bug_command_handler(client, message):
     # Check if the command is a reply
     if message.reply_to_message:
         # Get the message being replied to
         replied_message = message.reply_to_message
 
         # Extract message content
-        content = replied_message.text or replied_message.caption
+        content = replied_message.text or replied_message.caption or "No text content."
 
         # Check if there is media content
         media_type = (
@@ -29,16 +27,21 @@ def bug_command_handler(client, message):
         )
 
         # Prepare the report message with message link and media content info
-        report_message = f"**Bug reported by @{message.from_user.username}:**\n\n{content}\n\n{media_type}\n\n**Message Link:** {replied_message.link}"
+        report_message = (
+            f"**Bug reported by @{message.from_user.username}:**\n\n"
+            f"{content}\n\n"
+            f"{media_type}\n\n"
+            f"**Message Link:** {replied_message.link}"
+        )
 
         # Send the report message
-        client.send_message(
+        await client.send_message(
             Config.SUPPORT_ID, report_message, parse_mode=ParseMode.MARKDOWN
         )
     else:
         # If not a reply, send a message to reply with /bug command to report a bug
-        client.send_message(
+        await client.send_message(
             message.chat.id,
-            "To report a bug, please reply to the message with **/ bug** cmd.",
+            "To report a bug, please reply to the message with **/bug** command.",
             parse_mode=ParseMode.MARKDOWN,
         )
