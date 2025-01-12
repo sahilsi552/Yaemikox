842e6e2d67812d586f70af8e40deeb1bc8e361f9
SAHIL SURYAVANSHI <sahilsingh56701@gmail.com>
2025-01-12 15:33:53 +0530

    Add files via upload

--- Mikobot/plugins/info.py
+++ Mikobot/plugins/info.py
@@ -1,4 +1,4 @@
-       # <============================================== IMPORTS =========================================================>
+# <============================================== IMPORTS =========================================================>
 import os
 import re
 from html import escape
@@ -16,19 +16,20 @@ from telegram.ext import CommandHandler, ContextTypes
 from telegram.helpers import mention_html
 
 from Database.mongodb.approve_db import is_approved
-from Infamous.karma import START_IMG, SUPPORT_CHAT
-from Mikobot import DEV_USERS, DRAGONS, INFOPIC, OWNER_ID, function, BOT_NAME, DEMONS, TIGERS, WOLVES
+from Infamous.karma import START_IMG,SUPPORT_CHAT
+from Mikobot import DEV_USERS, DRAGONS, INFOPIC, OWNER_ID, function,BOT_NAME,DEMONS, TIGERS, WOLVES
 from Mikobot.__main__ import STATS, USER_INFO
 from Mikobot.plugins.helper_funcs.chat_status import support_plus
 from Mikobot.plugins.users import get_user_id
-from Mikobot import BOT_NAME, app
+
 from unidecode import unidecode
 from PIL import Image, ImageChops, ImageDraw, ImageFont
 from pyrogram import filters
 from pyrogram.enums import ParseMode
 
 
-# <============================================== IMAGE PROCESSING ==================================================>
+
+
 async def circle(pfp, size=(900, 900)):
     pfp = pfp.resize(size, Image.LANCZOS).convert("RGBA")
     bigsize = (pfp.size[0] * 3, pfp.size[1] * 3)
@@ -106,7 +107,7 @@ async def userinfopic(
     return userinfo
 
 
-# <============================================== COMMAND HANDLERS ==================================================>
+# Command handler for /userinfo
 @app.on_message(filters.command("uinfo"))
 async def userinfo_command(client, message):
     user = message.from_user
@@ -181,7 +182,6 @@ async def userinfo_command(client, message):
 
     except Exception as e:
         print(e)
-
 
 # <================================================ FUNCTION =======================================================>
 async def info(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -351,7 +351,6 @@ async def info(update: Update, context: ContextTypes.DEFAULT_TYPE):
 
 
 
-
 async def stats(update: Update, context: ContextTypes.DEFAULT_TYPE):
     ID = update.effective_message.from_user.id
     if ID != OWNER_ID:
@@ -377,189 +376,13 @@ async def stats(update: Update, context: ContextTypes.DEFAULT_TYPE):
         parse_mode=ParseMode.HTML,
         reply_markup=reply_markup,
     )
-     
+
 
 # <=================================================== HELP ====================================================>
 
 
 __help__ = """
 *Overall information about user:*
-        draw.text((user_id_x, user_id_y), str(user.id), font=font, fill="white")
-
-        userinfo = f"downloads/userinfo_{user.id}.png"
-        background.save(userinfo)
-
-    except Exception as e:
-        print(f"Error: {e}")
-        userinfo = None
-
-    return userinfo
-
-
-# Command handler for /userinfo
-@app.on_message(filters.command("uinfo"))
-async def userinfo_command(client, message):
-    user = message.from_user
-    user_x, user_y = 1035, 2885
-    user_id_x, user_id_y = 1035, 2755
-
-    try:
-        # Send a message indicating that user information is being processed
-        processing_message = await message.reply("Processing user information...")
-
-        # Generate user info image
-        image_path = await userinfopic(user, user_x, user_y, user_id_x, user_id_y)
-
-        # Delete the processing message
-        await processing_message.delete()
-
-        if image_path:
-            # Initialize the caption with basic information
-            caption = (
-                f"「 **According to the Mikos analogy, the userinfo is...** : 」\n\n"
-                f"❐  𝗜𝗗: {user.id}\n"
-                f"❐  𝗙𝗶𝗿𝘀𝘁 𝗡𝗮𝗺𝗲: {user.first_name}\n"
-                f"❐  𝗟𝗮𝘀𝘁 𝗡𝗮𝗺𝗲: {user.last_name}\n"
-                f"❐  𝗨𝘀𝗲𝗿𝗻𝗮𝗺𝗲: {user.username}\n"
-                f"❐  𝗨𝘀𝗲𝗿𝗹𝗶𝗻𝗸: [link](https://t.me/{user.username})\n"
-            )
-
-            # Check if the user's ID matches one of the predefined ranks
-            if user.id == OWNER_ID:
-                caption += "\n\n〄 The disaster level of this user is **Owner**.\n"
-            elif user.id in DEV_USERS:
-                caption += "\n\n〄 This user is a member of **Developer**.\n"
-            elif user.id in DRAGONS:
-                caption += "\n\n〄 The disaster level of this user is **Sudo**.\n"
-            elif user.id in DEMONS:
-                caption += "\n\n〄 The disaster level of this user is **Demon**.\n"
-            elif user.id in TIGERS:
-                caption += "\n\n〄 The disaster level of this user is **Tiger**.\n"
-            elif user.id in WOLVES:
-                caption += "\n\n〄 The disaster level of this user is **Wolf**.\n"
-
-            # Add the RANK line only if the user's ID matches one of the predefined ranks
-            if (
-                user.id == OWNER_ID
-                or user.id in DEV_USERS
-                or user.id in DRAGONS
-                or user.id in DEMONS
-                or user.id in TIGERS
-                or user.id in WOLVES
-            ):
-                caption += "\n\n〄 𝗥𝗮𝗻𝗸: "
-
-                if user.id == OWNER_ID:
-                    caption += "**CREATOR**"
-                elif user.id in DEV_USERS:
-                    caption += "**DEVELOPER**"
-                elif user.id in DRAGONS:
-                    caption += "**DRAGON**"
-                elif user.id in DEMONS:
-                    caption += "**DEMON**"
-                elif user.id in TIGERS:
-                    caption += "**TIGER**"
-                elif user.id in WOLVES:
-                    caption += "**WOLF**"
-
-                caption += "\n"
-
-            await message.reply_photo(
-                photo=image_path, caption=caption, parse_mode=ParseMode.MARKDOWN
-            )
-            os.remove(image_path)
-
-    except Exception as e:
-        print(e)
-
-# <================================================ FUNCTION =======================================================>
-async def info(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    chat = update.effective_chat
-
-        if chat_obj.username:
-            head = f"⇨【 <b>USER INFORMATION</b> 】⇦\n\n"
-            if chat_obj.username.endswith("bot"):
-                head = f"⇨【 <b>BOT INFORMATION</b> 】⇦\n\n"
-
-        head += f"➲ <b>ID:</b> <code>{chat_obj.id}</code>"
-        head += f"\n➲ <b>First Name:</b> {chat_obj.first_name}"
-        if chat_obj.last_name:
-            head += f"\n➲ <b>Last Name:</b> {chat_obj.last_name}"
-        if chat_obj.username:
-            head += f"\n➲ <b>Username:</b> @{chat_obj.username}"
-        head += f"\n➲ <b>Permalink:</b> {mention_html(chat_obj.id, 'link')}"
-
-        if chat_obj.username and not chat_obj.username.endswith("bot"):
-            head += f"\n\n💎 <b>Premium User:</b> {premium}"
-
-        if chat_obj.bio:
-            head += f"\n\n<b>➲ Bio:</b> {chat_obj.bio}"
-
-        chat_member = await chat.get_member(chat_obj.id)
-        if isinstance(chat_member, ChatMemberAdministrator):
-            head += f"\n➲ <b>Presence:</b> {chat_member.status}"
-            if chat_member.custom_title:
-                head += f"\n➲ <b>Admin Title:</b> {chat_member.custom_title}"
-        else:
-            head += f"\n➲ <b>Presence:</b> {chat_member.status}"
-
-        if is_approved(chat.id, chat_obj.id):
-            head += f"\n➲ <b>Approved:</b> This user is approved in this chat."
-
-        )
-
-
-
-
-async def stats(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    ID = update.effective_message.from_user.id
-    if ID != OWNER_ID:
-        return
-    stats = f"📊 <b>{BOT_NAME} Bot's Statistics:</b>\n\n" + "\n".join(
-        [mod.__stats__() for mod in STATS]
-    )
-    result = re.sub(r"(\d+)", r"<code>\1</code>", stats)
-
-    keyboard = [
-        [
-            InlineKeyboardButton(
-                "support", url=f"https://t.me/{SUPPORT_CHAT}"
-            ),
-        ]
-    ]
-
-    reply_markup = InlineKeyboardMarkup(keyboard)
-
-    await update.effective_message.reply_photo(
-        photo=str(choice(START_IMG)),
-        caption=result,
-        parse_mode=ParseMode.HTML,
-        reply_markup=reply_markup,
-    )
-
-
-# <=================================================== HELP ====================================================>
-
-
-__help__ = """
-#*Overall information about user:*
-
-#» /info : Fetch user information.
-
-#» /uinfo : Fetch user information in banner.
-"""
-
-# <================================================ HANDLER =======================================================>
-STATS_HANDLER = CommandHandler(["stats", "gstats"], stats, block=False)
-INFO_HANDLER = CommandHandler(("info", "book"), info, block=False)
-
-function(STATS_HANDLER)
-function(INFO_HANDLER)
-
-__mod_name__ = "Iɴꜰᴏ"
-__command_list__ = ["info"]
-__handlers__ = [INFO_HANDLER, STATS_HANDLER]
-# <================================================ END =======================================================>
 
 » /info : Fetch user information.
 
