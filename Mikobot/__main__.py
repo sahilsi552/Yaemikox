998d25fc03d92321de10872b27c54c4a3e988d1d
SAHIL SURYAVANSHI <sahilsingh56701@gmail.com>
2025-01-13 10:28:51 +0530

    Update __main__.py

--- Mikobot/__main__.py
+++ Mikobot/__main__.py
@@ -325,7 +325,8 @@ async def extra_command_callback(update: Update, context: ContextTypes.DEFAULT_T
             parse_mode="Markdown",  # Added this line to explicitly specify Markdown parsing
         )
 async def Music_button(update:Update,context:ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(f"""ᴄʜᴏꜱᴇ ᴛʜᴇ ᴄᴀᴛᴇɢᴏʀʏ ꜰᴏʀ ᴡʜɪᴄʜ ʏᴏᴜ ᴡᴀɴɴᴀ ɢᴇᴛ ʜᴇʟᴘ.
+    await update.message.reply_text(
+        """ᴄʜᴏꜱᴇ ᴛʜᴇ ᴄᴀᴛᴇɢᴏʀʏ ꜰᴏʀ ᴡʜɪᴄʜ ʏᴏᴜ ᴡᴀɴɴᴀ ɢᴇᴛ ʜᴇʟᴘ.
 ᴀꜱᴋ ʏᴏᴜʀ ᴅᴏᴜʙᴛꜱ ᴀᴛ [ꜱᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ](https://t.me/{SUPPORT_CHAT})
 
 ᴀʟʟ ᴄᴏᴍᴍᴀɴᴅꜱ ᴄᴀɴ ʙᴇ ᴜꜱᴇᴅ ᴡɪᴛʜ : /  """,
@@ -381,7 +382,7 @@ async def Music_about_callback(update: Update, context: ContextTypes.DEFAULT_TYP
     query = update.callback_query
     if query.data == "Music_":
         await query.message.edit_text(
-            f"""
+            """
  ᴄʜᴏꜱᴇ ᴛʜᴇ ᴄᴀᴛᴇɢᴏʀʏ ꜰᴏʀ ᴡʜɪᴄʜ ʏᴏᴜ ᴡᴀɴɴᴀ ɢᴇᴛ ʜᴇʟᴘ.
 ᴀꜱᴋ ʏᴏᴜʀ ᴅᴏᴜʙᴛꜱ ᴀᴛ [ꜱᴜᴘᴘᴏʀᴛ ᴄʜᴀᴛ](https://t.me/{SUPPORT_CHAT})
 
@@ -609,7 +610,7 @@ force : sᴛᴀɴᴅs ғᴏʀ ғᴏʀᴄᴇ ᴩʟᴀʏ.
             ),
         )
     elif query.data == "Music_song":
-        await query.message.edit_text("music mode text hereeeee"
+        await query.message.edit_text(
          """*»ᴄᴏᴍᴍᴀɴᴅꜱ «*
 sᴏɴɢ ᴅᴏᴡɴʟᴏᴀᴅ
 
